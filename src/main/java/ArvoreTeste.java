7b7f677466413f726ed0ad8670976210aacf7df8
obrion333 <diogogameiro23@gmail.com>
2025-11-28 10:43:07 +0000

    Feita a class Trie, a inner class TrieNode, metodos insert e imprimirArvore na class Trie

---
+++ src/main/java/ArvoreTeste.java
@@ -0,0 +1,17 @@
+public class ArvoreTeste {
+
+
+    public static void main(String[] args) {
+
+
+        Trie arvore = new Trie();
+
+        arvore.insert("bna");
+        arvore.insert("macaco");
+        arvore.insert("macacodois");
+        arvore.insert("joao");
+        arvore.imprimirArvore();
+
+    }
+
+}
--- src/main/java/Trie.java
+++ src/main/java/Trie.java
@@ -2,6 +2,82 @@ public class Trie {
 
 
 
+private static class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    boolean isWord;
+
+    TrieNode() {
+        // iniciar todos os children a null, limpar
+        for (int i = 0; i < 26; i++){
+            children[i] = null;
+        }
+        isWord = false;
+    }
+
+}
+
+
+private final TrieNode root;
+
+public Trie(){
+    root = new TrieNode();
+}
+
+public void insert(String word){
+
+    // começar na root sempre
+    TrieNode node = root;
+
+    // percorrer todos os caracteres da palavra
+    for (char c : word.toCharArray()){
+
+        int index = c - 'a';
+
+        //se root.children for null, vamos criar o TreeNode();
+        if(node.children[index] == null){
+            // index é a posiçao no alfabeto
+            node.children[index] = new TrieNode();
+
+        }
+
+        //passamos para o node de baixo
+        node = node.children[index];
+    }
+
+    //depois de acabarmos a palavra, marcamos o node como acabado, isWord = true
+    node.isWord = true;
+
+    }
+
+
+
+
+    public void imprimirArvore(){
+    imprimirRecursivo(root, "");
+    }
+
+
+    private void imprimirRecursivo(TrieNode node, String prefixo){
+
+    if(node.isWord){
+        System.out.println(prefixo);
+    }
+
+    // percorrer os 26 filhos
+        for (int i = 0; i<26; i++) {
+
+            TrieNode filho = node.children[i];
+            if (filho != null){
+                char letra = (char) ('a' + i);
+                imprimirRecursivo(filho, prefixo + letra);
+            }
+
+        }
+    }
+
+
+
+
 
 
 
